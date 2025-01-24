da3578d0f0d83176450401f7ef3b526bb00c57ee
Lazy Monad <lazymonad@Lazys-MacBook-Air.local>
2025-01-24 08:50:53 +0200

    Add colors to python graph server

--- graph_server.py
+++ graph_server.py
@@ -1,13 +1,24 @@
 from dash import Dash, html
 import dash_cytoscape as cyto
 import pandas as pd
+import sys
 app = Dash(__name__)
 
-df = pd.read_csv("./out.csv")
-data = [{"data": {"source": row["hash"], "target": f'{row["url"]}({row["code"]})'}} for _, row in df.iterrows()]
-edges_source = [{"data": {"id": row["hash"], "label": row["hash"]}} for _, row in df.iterrows()]
-edges_target = [{"data": {"id": f'{row["url"]}({row["code"]})', "label": f'{row["url"]}({row["code"]})'}}
+df = pd.read_csv(sys.argv[1])
+data = [{"data": {"source": f'{row["hash"]}({row["bytes"]})', "target": f'{row["url"]}({row["code"]})'}} for _, row in df.iterrows()]
+edges_source = [{"data": {"id": f'{row["hash"]}({row["bytes"]})', "label": f'{row["hash"]}({row["bytes"]})'},"classes":"green"} for _, row in df.iterrows()]
+
+edges_target = [
+    {
+        "data": {
+            "id": f'{row["url"]}({row["code"]})',
+            "label": f'{row["url"]}({row["code"]})',
+        },
+        "classes": "red" if row["code"] == 404 or row["code"] == 403 else "green",
+    }
     for _, row in df.iterrows()]
+
+
 result = edges_source + edges_target + data
 
 app.layout = html.Div([
@@ -15,8 +26,30 @@ app.layout = html.Div([
     cyto.Cytoscape(
         id='cytoscape',
         elements=result, 
-        layout={'name': 'breadthfirst'},
-        style={'width': '1200px', 'height': '800px'}
+        layout={'name': 'breadthfirst', 'circle':True},
+        style={'width': '100%', 'height': '800px'},
+        stylesheet=[
+            {
+                'selector':'node',
+                'style': {
+                    'content':'data(label)'
+                }
+            },
+            {
+                'selector':'.green',
+                'style':{
+                    'background-color':'green',
+                    'line-color':'green'
+                }
+            },
+            {
+                'selector':'.red',
+                'style':{
+                    'background-color':'red',
+                    'line-color':'red'
+                }
+            }
+        ]
     )
 ])
 
